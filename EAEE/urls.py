77988fd969c846af0c1416415bc97ef15d865ad8
francisco-duo <francisco.duo2000@gmail.com>
2023-11-04 09:34:53 -0300

    feat: Criando url para direcionar para a pagia do usuario logado

--- EAEE/urls.py
+++ EAEE/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, app_login
+from .views import home_page, app_login, user_page
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('login/', app_login, name='login'),
+    path('usuario/<str:user_name>', user_page, name='access'),
 ]
