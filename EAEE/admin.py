d483d776448c6bae6fac72eb1b1af614f3cf3488
francisco-duo <francisco.duo2000@gmail.com>
2024-01-09 21:16:43 -0300

    Financeiro e models do Admin

--- EAEE/admin.py
+++ EAEE/admin.py
@@ -2,17 +2,37 @@ from django.contrib import admin
 from EAEE.models import *
 
 
+class DevolutivaAdmin(admin.ModelAdmin):
+    list_display = (
+        'paciente', 'dt_devolutiva'
+    )
+
+
+class ReuniaoExternaAdmin(admin.ModelAdmin):
+    list_display = (
+        'paciente', 'dt_devolutiva'
+    )
+
+
+class EncaminhamentoAdmin(admin.ModelAdmin):
+    list_display = (
+        'paciente', 'dt_devolutiva'
+    )
+
+
+class EvolucaoDiariaAdmin(admin.ModelAdmin):
+    list_display = (
+        'paciente', 'dt_devolutiva'
+    )
+
+
 admin.site.register(PacienteModel)
 admin.site.register(TipoDeVinculoComPacienteModels)
 admin.site.register(InformacoesComplementares)
-# from .models import PacienteModel, PacienteRegistroModel, RegistroFinanceiroTipoModel
-# admin.site.register(PermissoesModel)
-
-# # Register your models here.
 admin.site.register(RegistroFinanceiroModel)
 admin.site.register(RegistroFinanceiroTipoModel)
 
-# admin.site.register(DevolutivaModel)
-# admin.site.register(ReunioesExternasModel)
-# admin.site.register(EncaminhamentoModel)
-# admin.site.register(EvolucaoDiariaModel)
+admin.site.register(DevolutivaModel, DevolutivaAdmin)
+admin.site.register(ReunioesExternasModel, ReuniaoExternaAdmin)
+admin.site.register(EncaminhamentoModel, EncaminhamentoAdmin)
+admin.site.register(EvolucaoDiariaModel, EvolucaoDiariaAdmin)
--- EAEE/models/model_financeiro.py
+++ EAEE/models/model_financeiro.py
@@ -19,12 +19,19 @@ class RegistroFinanceiroModel(models.Model):
     registro_financeiro_tipo: ForeignKey
     """
 
+    class Meta:
+        verbose_name = "Financeiro"
+        verbose_name_plural = "Financeiro"
+
     registro_financeiro_funcionario = models.ForeignKey(User, blank=True, on_delete=models.DO_NOTHING)
     registro_financeiro_valor = models.CharField(max_length=255, blank=True)
     registro_financeiro_destino = models.CharField(max_length=255, blank=True)
     registro_financeiro_tipo = models.ForeignKey(RegistroFinanceiroTipoModel, on_delete=models.DO_NOTHING)
     registro_financeiro_dt = models.DateTimeField(verbose_name='Data')
     data_filtro = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.registro_financeiro_destino
 
     def save(self, *args, **kwargs):
         self.data_filtro = self.registro_financeiro_dt.date()
