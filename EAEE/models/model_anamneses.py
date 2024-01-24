80d10d731e315aded7a4b3e24e7a684789e32f10
francisco-duo <francisco.duo2000@gmail.com>
2024-01-23 23:09:27 -0300

    feat: Adicionando verbose_names nos campos das anamneses

--- EAEE/models/model_anamneses.py
+++ EAEE/models/model_anamneses.py
@@ -13,32 +13,105 @@ class Psicopedagogia(models.Model):
     dt_registro = models.DateTimeField(default=timezone.now, verbose_name='Data')
     especialista = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     paciente = models.ForeignKey(model_pacientes.PacienteModel, on_delete=models.CASCADE)
-    idade_que_comecou_a_frequentar_escola = models.CharField(blank=True, null=True, max_length=255)
-    como_foi_adaptacao = models.TextField(blank=True, null=True)
-    reprovou = models.CharField(blank=True, null=True, max_length=255)
-    ralacao_professores_colegas = models.TextField(blank=True, null=True)
-    motivado_escola = models.CharField(blank=True, null=True, max_length=255)
-    falta_assiduo = models.TextField(blank=True, null=True)
-    realiza_atividades_com_satisfacao = models.CharField(blank=True, null=True, max_length=255)
-    precisa_ajuda = models.TextField(blank=True, null=True)
-    predilecao_matematica_portugues = models.TextField(blank=True, null=True)
-    dificuldades_aprendizagem = models.TextField(blank=True, null=True)
-    antecedentes_com_dificuldade = models.TextField(blank=True, null=True)
-    queixas_comuns_escola = models.TextField(blank=True, null=True)
-    fala_compreensivel = models.TextField(blank=True, null=True)
+
+    idade_que_comecou_a_frequentar_escola = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Com que idade começou a frequentar a escola?'
+    )
+    como_foi_adaptacao = models.TextField(
+        blank=True, null=True,
+        verbose_name='Como foi a adaptação?'
+    )
+    reprovou = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Reprovou? Qual Série?'
+    )
+    ralacao_professores_colegas = models.TextField(
+        blank=True, null=True,
+        verbose_name='Como se relaciona com os colegas e professores?'
+    )
+    motivado_escola = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Se sente motivado a ir para escola? Sim ou Não?'
+    )
+    falta_assiduo = models.TextField(
+        blank=True, null=True,
+        verbose_name='Assíduo ou falta com frequência?'
+    )
+    realiza_atividades_com_satisfacao = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Realiza as atividades de casa com satisfação? Sim ou Não?'
+    )
+    precisa_ajuda = models.TextField(
+        blank=True, null=True,
+        verbose_name='Precisa de suporte ou é independente ao realizar?'
+    )
+    predilecao_matematica_portugues = models.TextField(
+        blank=True, null=True,
+        verbose_name='Demonstra predileção por matemática ou língua portuguesa?'
+    )
+    dificuldades_aprendizagem = models.TextField(
+        blank=True, null=True,
+        verbose_name='Que dificuldades de aprendizagem você observa?'
+    )
+    antecedentes_com_dificuldade = models.TextField(
+        blank=True, null=True,
+        verbose_name='Na família, há antecedentes com dificuldade?'
+    )
+    queixas_comuns_escola = models.TextField(
+        blank=True, null=True,
+        verbose_name='Queixas comuns da escola?'
+    )
+    fala_compreensivel = models.TextField(
+        blank=True, null=True,
+        verbose_name='Fala comppreensível? Observa troca ou omissões de fonemas? Conta uma história com começo, meio e fim?'
+    )
     # troca_ou_omissao_fonemas = models.TextField(blank=True, null=True)
     # conta_historia = models.TextField(blank=True, null=True)
-    corporalmente_desenvolto = models.CharField(blank=True, null=True, max_length=255)
-    anda_bicicleta = models.CharField(blank=True, null=True, max_length=255)
-    organizado = models.CharField(blank=True, null=True, max_length=255)
-    ajuda_em_casa = models.TextField(blank=True, null=True)
-    censurado = models.TextField(blank=True, null=True)
-    brinquedos_favorito = models.TextField(blank=True, null=True)
-    gosta_desenhar = models.TextField(blank=True, null=True)
-    uso_de_eletronicos = models.TextField(blank=True, null=True)
-    horas_livres = models.TextField(blank=True, null=True)
-    potencialidades_marcantes = models.TextField(blank=True, null=True)
-    fragilidades_evidentes = models.TextField(blank=True, null=True)
+    corporalmente_desenvolto = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='É corporalmente desenvolto?'
+    )
+    anda_bicicleta = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Anda de bicicleta, patins, patinete...?'
+    )
+    organizado = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Organizado com seus pertences? Sim ou Não?'
+    )
+    ajuda_em_casa = models.TextField(
+        blank=True, null=True,
+        verbose_name='Ajuda na organização da casa? Que afazeres são de sua responsabilidade?'
+    )
+    censurado = models.TextField(
+        blank=True, null=True,
+        verbose_name='É muito censurado?'
+    )
+    brinquedos_favorito = models.TextField(
+        blank=True, null=True,
+        verbose_name='Brinquedos/Brincadeiras/Personagens/Programas de TV favoritos'
+    )
+    gosta_desenhar = models.TextField(
+        blank=True, null=True,
+        verbose_name='Gosta de desenhar, colorir, modelar?'
+    )
+    uso_de_eletronicos = models.TextField(
+        blank=True, null=True,
+        verbose_name='Tempo diário de uso em eletrônicos (celulares, tablets e afins)'
+    )
+    horas_livres = models.TextField(
+        blank=True, null=True,
+        verbose_name='O que gosta de fazer nas horas livres?'
+    )
+    potencialidades_marcantes = models.TextField(
+        blank=True, null=True,
+        verbose_name='Potencialidades marcantes?'
+    )
+    fragilidades_evidentes = models.TextField(
+        blank=True, null=True,
+        verbose_name='Fragilidades evidentes'
+    )
 
     def __str__(self, ):
         return self.paciente
@@ -48,56 +121,199 @@ class Psicologia(models.Model):
     class Meta:
         verbose_name = "Psicologia"
         verbose_name_plural = "Psicologia"
+
     dt_registro = models.DateTimeField(default=timezone.now, verbose_name='Data')
     especialista = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     paciente = models.ForeignKey(model_pacientes.PacienteModel, on_delete=models.CASCADE)
-    numero_irmaos = models.CharField(blank=True, null=True, max_length=255)
-    reacao_nascimento_irmao = models.TextField(blank=True, null=True)
-    relacionamento_irmaos = models.TextField(blank=True, null=True)
-    relacionamento_pais = models.TextField(blank=True, null=True)
-    relacionamento_familia = models.TextField(blank=True, null=True)
-    personalidade = models.CharField(blank=True, null=True, max_length=255)
-    comportamento_roer_unhas = models.TextField(blank=True, null=True)
-    humor_habitual = models.TextField(blank=True, null=True)
-    brinca_sozinho_ou_grupo = models.TextField(blank=True, null=True)
-    estranha_mudancas_ambiente = models.TextField(blank=True, null=True) 
-    aceita_seguir_comandos = models.TextField(blank=True, null=True)
-    brincadeiras_brinquedo_favorito = models.TextField(blank=True, null=True)
-    gosta_de_musicas = models.TextField(blank=True, null=True)
-    baste_cabeca_na_parede = models.TextField(blank=True, null=True)
-    atitudes_desse_habito = models.TextField(blank=True, null=True)
-    reprovou_na_escola = models.TextField(blank=True, null=True)
-    relacionamento_colegas = models.TextField(blank=True, null=True)
-    relacionamento_professores = models.TextField(blank=True, null=True)
-    idade_desfralde = models.TextField(blank=True, null=True)
-    dificuldades_desfraude = models.TextField(blank=True, null=True)
-    higiene_sozinho = models.TextField(blank=True, null=True)
-    idade_higiene_sozinho = models.TextField(blank=True, null=True)
-    problema_na_fala = models.TextField(blank=True, null=True)
-    troca_letras = models.TextField(blank=True, null=True)
-    acorda_vai_para_cama_dos_pais = models.TextField(blank=True, null=True)
-    dorme_sozinho = models.TextField(blank=True, null=True)
-    objeto_para_dormir = models.TextField(blank=True, null=True)
-    urina_na_cama = models.TextField(blank=True, null=True)
-    horario_para_refeicoes = models.TextField(blank=True, null=True)
-    refeicoes_em_familia = models.TextField(blank=True, null=True)
-    local = models.TextField(blank=True, null=True)
-    assiste_enquanto_come = models.TextField(blank=True, null=True)
-    como_se_alimenta = models.TextField(blank=True, null=True)
-    seletividade_alimentar = models.TextField(blank=True, null=True)
-    curiosidade_sexual = models.TextField(blank=True, null=True)
-    atitude_dos_pais = models.TextField(blank=True, null=True)
-    informacoes_sexuais = models.TextField(blank=True, null=True)
-    tratamento_medico = models.TextField(blank=True, null=True)
-    internado = models.TextField(blank=True, null=True)
-    motivo_internação = models.TextField(blank=True, null=True)
-    operado = models.TextField(blank=True, null=True)
-    idade_operado = models.TextField(blank=True, null=True)
-    adoece_facilmente = models.TextField(blank=True, null=True)
-    acompanhamento_psicologico = models.TextField(blank=True, null=True)
-    encaminhamento = models.CharField(blank=True, null=True, max_length=255)
-    algum_membro_ja_fez_acompanhamento_psicologico = models.TextField(blank=True, null=True)
-    queixa_psicologica_principal = models.TextField(blank=True, null=True)
+
+    numero_irmaos = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Número de irmãos'
+    )
+    reacao_nascimento_irmao = models.TextField(
+        blank=True, null=True,
+        verbose_name='Reação da criança em relação ao nascimento de irmãos mais novos'
+    )
+    relacionamento_irmaos = models.TextField(
+        blank=True, null=True,
+        verbose_name='Relacionamento com os irmãos'
+    )
+    relacionamento_pais = models.TextField(
+        blank=True, null=True,
+        verbose_name='Relacionamento com os pais'
+    )
+    relacionamento_familia = models.TextField(
+        blank=True, null=True,
+        verbose_name='Relacionamento com a família materna e paterna'
+    )
+    personalidade = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Personalidade'
+    )
+    comportamento_roer_unhas = models.TextField(
+        blank=True, null=True,
+        verbose_name='Comportamento de roer unhas/ Chupar dedos/ Tique'
+    )
+    humor_habitual = models.TextField(
+        blank=True, null=True,
+        verbose_name='Humor habitual'
+    )
+    brinca_sozinho_ou_grupo = models.TextField(
+        blank=True, null=True,
+        verbose_name='Prefere brincar sozinho ou em grupo?'
+    )
+    estranha_mudancas_ambiente = models.TextField(
+        blank=True, null=True,
+        verbose_name='Estranha mudanças no ambiente?'
+    ) 
+    aceita_seguir_comandos = models.TextField(
+        blank=True, null=True,
+        verbose_name='Aceita seguir comandos?'
+    )
+    brincadeiras_brinquedo_favorito = models.TextField(
+        blank=True, null=True,
+        verbose_name='Brinquedos e brincadeiras preferidas?'
+    )
+    gosta_de_musicas = models.TextField(
+        blank=True, null=True,
+        verbose_name='Gosta de música? Quais'
+    )
+    baste_cabeca_na_parede = models.TextField(
+        blank=True, null=True,
+        verbose_name='Bate a cabeça na parede?'
+    )
+    atitudes_desse_habito = models.TextField(
+        blank=True, null=True,
+        verbose_name='Que atitudes são tomadas diante desse hábito'
+    )
+    reprovou_na_escola = models.TextField(
+        blank=True, null=True,
+        verbose_name='Reprovou na escola?'
+    )
+    relacionamento_colegas = models.TextField(
+        blank=True, null=True,
+        verbose_name='Relacionamento com os colegas das escola?'
+    )
+    relacionamento_professores = models.TextField(
+        blank=True, null=True,
+        verbose_name='Relacionamento com os professores da escola?'
+    )
+    idade_desfralde = models.TextField(
+        blank=True, null=True,
+        verbose_name='Com que idade ocorreu o desfraude?'
+    )
+    dificuldades_desfraude = models.TextField(
+        blank=True, null=True,
+        verbose_name='Apresentou dificuldades? Se sim, qual a posição dos pais?'
+    )
+    higiene_sozinho = models.TextField(
+        blank=True, null=True,
+        verbose_name='Faz higiene sozinho?'
+    )
+    idade_higiene_sozinho = models.TextField(
+        blank=True, null=True,
+        verbose_name='Se sim, começou com que idade?'
+    )
+    problema_na_fala = models.TextField(
+        blank=True, null=True,
+        verbose_name='Apresenta alguma problemma ao falar?'
+    )
+    troca_letras = models.TextField(
+        blank=True, null=True,
+        verbose_name='Troca letras?'
+    )
+    acorda_vai_para_cama_dos_pais = models.TextField(
+        blank=True, null=True,
+        verbose_name='Quando acorda vai para a cama dos pais?'
+    )
+    dorme_sozinho = models.TextField(
+        blank=True, null=True,
+        verbose_name='Medo de dormir sozinho?'
+    )
+    objeto_para_dormir = models.TextField(
+        blank=True, null=True,
+        verbose_name='Utiliza algum objeto para dormir?'
+    )
+    urina_na_cama = models.TextField(
+        blank=True, null=True,
+        verbose_name='Costuma urinar na cama?'
+    )
+    horario_para_refeicoes = models.TextField(
+        blank=True, null=True,
+        verbose_name='Tem horário para as refeições?'
+    )
+    refeicoes_em_familia = models.TextField(
+        blank=True, null=True,
+        verbose_name='Faz as refeições com a família?'
+    )
+    local = models.TextField(
+        blank=True, null=True,
+        verbose_name='Em que local?'
+    )
+    assiste_enquanto_come = models.TextField(
+        blank=True, null=True,
+        verbose_name='Enquanto come, assiste a TV ou mexe em eletrônicos?'
+    )
+    como_se_alimenta = models.TextField(
+        blank=True, null=True,
+        verbose_name='Como se alimenta (rápido, devagar, mastiga bem, ...)?'
+    )
+    seletividade_alimentar = models.TextField(
+        blank=True, null=True,
+        verbose_name='Apresenta seletividade alimentar?'
+    )
+    curiosidade_sexual = models.TextField(
+        blank=True, null=True,
+        verbose_name='Apresentou curiosidade sexual?'
+    )
+    atitude_dos_pais = models.TextField(
+        blank=True, null=True,
+        verbose_name='Qual a atitude dos pais?'
+    )
+    informacoes_sexuais = models.TextField(
+        blank=True, null=True,
+        verbose_name='Acha importante das informações sexuais? Por que?'
+    )
+    tratamento_medico = models.TextField(
+        blank=True, null=True,
+        verbose_name='Faz algum tratamento médico'
+    )
+    internado = models.TextField(
+        blank=True, null=True,
+        verbose_name='Já esteve internado?'
+    )
+    motivo_internação = models.TextField(
+        blank=True, null=True,
+        verbose_name='Motivo da internação?'
+    )
+    operado = models.TextField(
+        blank=True, null=True,
+        verbose_name='Ja foi operado?'
+    )
+    idade_operado = models.TextField(
+        blank=True, null=True,
+        verbose_name='Com que idade?'
+    )
+    adoece_facilmente = models.TextField(
+        blank=True, null=True,
+        verbose_name='Adoece facilente?'
+    )
+    acompanhamento_psicologico = models.TextField(
+        blank=True, null=True,
+        verbose_name='Fez acompanhamento psicológico antes?'
+    )
+    encaminhamento = models.CharField(
+        blank=True, null=True, max_length=255,
+        verbose_name='Encaminhamento?'
+    )
+    algum_membro_ja_fez_acompanhamento_psicologico = models.TextField(
+        blank=True, null=True,
+        verbose_name='Algum membro da família faz ou fez acompanhamento psicológico?'
+    )
+    queixa_psicologica_principal = models.TextField(
+        blank=True, null=True,
+        verbose_name='Queixa psicológica principal'
+    )
 
     def __str__(self, ):
         return self.paciente
